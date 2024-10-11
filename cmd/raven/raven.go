6e33e733d810107771ed44958d2c9e03304d35f0
Sepehr Rajabi <sepehrrajabi478@gmail.com>
2024-10-11 22:16:53 +0330

    implemented the main function

--- cmd/raven/raven.go
+++ cmd/raven/raven.go
@@ -1,6 +1,21 @@
 package main
 
-import ()
+import (
+	"log"
+	"net/http"
+
+	"github.com/ravenbox/raven-prototype"
+	"github.com/ravenbox/raven-prototype/pkg/sfu"
+)
 
 func main() {
+	sfu := sfu.NewSFU()
+	raven := raven.NewRaven(sfu)
+
+	log.Println("Starting server...")
+
+	err := http.ListenAndServe("127.0.0.1:8000", raven)
+	if err != nil {
+		log.Panicln("Bruh", err)
+	}
 }
